3ae746f9c08bb0ddde3380664f67135a510ec0ab
Anish <87910771+AnishLohiya@users.noreply.github.com>
2023-12-10 08:14:52 +0530

    scroll final setup

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 // export const reportAddress = "0xC6CBcd238d5c3b9B039bF9A2F75F67e0E3499D78";
-export const reportAddress = "0x5Eba497dE827E89Ac06c45E2333d2B08e001bbaC";
+// export const reportAddress = "0x5Eba497dE827E89Ac06c45E2333d2B08e001bbaC";
+export const reportAddress = "0x7Df1B3f6C15531d0cd14964B18d8D1953D1d3DcE";
